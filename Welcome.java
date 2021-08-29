4208095e60dc0ea8a5378c63b64e2c58092076ea
Ritik Chaurasiya <chaurasiyaritik50@gmail.com>
2021-08-29 13:02:14 +0530

    a-/-@

--- Welcome.java
+++ Welcome.java
@@ -1,5 +1,5 @@
 public class Start {
 	public static void main(String[] args){
-		System.out.println("Welcome To Employee Wage Computation!!");
+		System.out.println("Welcome To Employee W@ge Computation!!");
 	}
 }
